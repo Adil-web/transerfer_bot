efb4b157148c574a9ce7be25db28c1d8ff68ff3a
Adil_Zakarin <62048169+Adil-web@users.noreply.github.com>
2021-07-17 19:08:32 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const Bot = require('node-telegram-bot-api')
 const {summary, allPrinciples} = require('./summary')
 
@@ -20,53 +21,59 @@ const principles = [
     ['6. «Отпустить свою хватку и позволить миру двигаться по течению вариантов»'],
     ['7. «Всякое отражение воспринимать как позитивное»']]
 
-bot.onText(/\/start/, function onStartText(msg) {
-    const { id, first_name: userName } = msg.chat
-    const opts = {
-      reply_to_message_id: msg.message_id,
-      reply_markup: JSON.stringify({
-        keyboard: principles
-      })
-    };
-    bot.sendMessage(id, 'ПРИНЦИПЫ ТРАНСЕРФИНГА РЕАЛЬНОСТИ', opts);
-    bot.sendMessage(id, `Добро пожаловать ${userName}!`)
-});
+http.createServer((request, response) => {
+    console.log('request starting for ');
+    console.log(request);
+    bot.onText(/\/start/, function onStartText(msg) {
+        const { id, first_name: userName } = msg.chat
+        const opts = {
+          reply_to_message_id: msg.message_id,
+          reply_markup: JSON.stringify({
+            keyboard: principles
+          })
+        };
+        bot.sendMessage(id, 'ПРИНЦИПЫ ТРАНСЕРФИНГА РЕАЛЬНОСТИ', opts);
+        bot.sendMessage(id, `Добро пожаловать ${userName}!`)
+    });
+    
+    bot.on('message', msg => {
+        const { id } = msg.chat
+        if(/test/gi.test(msg.text)) {
+            setInterval(() => {
+                bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
+            }, (1000 * 60 * 60) * hours)
+        }   
+        if(/start/gi.test(msg.text)) {
+            return
+        }   
+        else if(principles[0][0]) {
+            bot.sendMessage(id, allPrinciples[0])
+        }
+        else if(principles[1][0]) {
+            bot.sendMessage(id, allPrinciples[1])
+        }
+        else if(principles[2][0]) {
+            bot.sendMessage(id, allPrinciples[2])
+        }
+        else if(principles[3][0]) {
+            bot.sendMessage(id, allPrinciples[3])
+        }
+        else if(principles[4][0]) {
+            bot.sendMessage(id, allPrinciples[4])
+        }
+        else if(principles[5][0]) {
+            bot.sendMessage(id, allPrinciples[5])
+        }
+        else if(principles[6][0]) {
+            bot.sendMessage(id, allPrinciples[6])
+        }
+        else if(/черный/gi.test(msg.text)) {
+            bot.sendSticker(id, 'CAACAgIAAxkBAAECl0tg8pGndFDpxClqPUXr9D5Iz-GOZwACuAADHEwXNf-uzxqylPi2IAQ')
+        }
+        else {
+            bot.sendMessage(id, 'Извините такой команды не существует...')
+        }
+    })
+}).listen(process.env.PORT || 5000)
 
-bot.on('message', msg => {
-    const { id } = msg.chat
-    if(/test/gi.test(msg.text)) {
-        setInterval(() => {
-            bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
-        }, (1000 * 60 * 60) * hours)
-    }   
-    if(/start/gi.test(msg.text)) {
-        return
-    }   
-    else if(principles[0][0]) {
-        bot.sendMessage(id, allPrinciples[0])
-    }
-    else if(principles[1][0]) {
-        bot.sendMessage(id, allPrinciples[1])
-    }
-    else if(principles[2][0]) {
-        bot.sendMessage(id, allPrinciples[2])
-    }
-    else if(principles[3][0]) {
-        bot.sendMessage(id, allPrinciples[3])
-    }
-    else if(principles[4][0]) {
-        bot.sendMessage(id, allPrinciples[4])
-    }
-    else if(principles[5][0]) {
-        bot.sendMessage(id, allPrinciples[5])
-    }
-    else if(principles[6][0]) {
-        bot.sendMessage(id, allPrinciples[6])
-    }
-    else if(/черный/gi.test(msg.text)) {
-        bot.sendSticker(id, 'CAACAgIAAxkBAAECl0tg8pGndFDpxClqPUXr9D5Iz-GOZwACuAADHEwXNf-uzxqylPi2IAQ')
-    }
-    else {
-        bot.sendMessage(id, 'Извините такой команды не существует...')
-    }
-})
+console.log('Server running at http://127.0.0.1:5000/');
