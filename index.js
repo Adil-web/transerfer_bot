3a52fd66d1766629db3b4713111cfb291704c74c
Adil_Zakarin <62048169+Adil-web@users.noreply.github.com>
2021-07-17 19:53:30 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ const principles = [
     ['6. «Отпустить свою хватку и позволить миру двигаться по течению вариантов»'],
     ['7. «Всякое отражение воспринимать как позитивное»']]
 
+http.createServer((request, response) => {
+    console.log('request starting for ');
+    console.log(request);
     bot.onText(/\/start/, function onStartText(msg) {
         const { id, first_name: userName } = msg.chat
         const opts = {
@@ -31,6 +34,9 @@ const principles = [
         };
         bot.sendMessage(id, 'ПРИНЦИПЫ ТРАНСЕРФИНГА РЕАЛЬНОСТИ', opts);
         bot.sendMessage(id, `Добро пожаловать ${userName}!`)
+        setInterval(() => {
+            bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
+        }, (1000 * 60 * 60) * hours)
     });
     
     bot.on('message', msg => {
@@ -39,35 +45,103 @@ const principles = [
             setInterval(() => {
                 bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
             }, (1000 * 60 * 60) * hours)
-        }   
+        }
         if(/start/gi.test(msg.text)) {
             return
-        }   
-        else if(principles[0][0]) {
-            bot.sendMessage(id, allPrinciples[0])
         }
-        else if(principles[1][0]) {
-            bot.sendMessage(id, allPrinciples[1])
-        }
-        else if(principles[2][0]) {
-            bot.sendMessage(id, allPrinciples[2])
-        }
-        else if(principles[3][0]) {
-            bot.sendMessage(id, allPrinciples[3])
-        }
-        else if(principles[4][0]) {
-            bot.sendMessage(id, allPrinciples[4])
-        }
-        else if(principles[5][0]) {
-            bot.sendMessage(id, allPrinciples[5])
-        }
-        else if(principles[6][0]) {
-            bot.sendMessage(id, allPrinciples[6])
-        }
-        else if(/черный/gi.test(msg.text)) {
-            bot.sendSticker(id, 'CAACAgIAAxkBAAECl0tg8pGndFDpxClqPUXr9D5Iz-GOZwACuAADHEwXNf-uzxqylPi2IAQ')
-        }
-        else {
-            bot.sendMessage(id, 'Извините такой команды не существует...')
-        }
+        switch (msg.text) {
+            case '1. «Мир, как зеркало, отражает ваше отношение к нему»':
+                bot.sendMessage(id, allPrinciples[0])
+                break;
+            case principles[1][0]:
+                bot.sendMessage(id, allPrinciples[1])
+                break
+            case principles[2][0]:
+                bot.sendMessage(id, allPrinciples[2])
+                break
+            case principles[3][0]:
+                bot.sendMessage(id, allPrinciples[3])
+                break
+            case principles[4][0]:
+                bot.sendMessage(id, allPrinciples[4])
+                break
+            case principles[5][0]:
+                bot.sendMessage(id, allPrinciples[5])
+                break
+            case principles[6][0]:
+                bot.sendMessage(id, allPrinciples[6])
+                break
+            case 'черный': 
+                break
+                default:
+                    bot.sendMessage(id, 'Извините такой команды не существует...')
+                    bot.sendMessage(id, msg.text)
+                    break;
+            }
+            if(/черный/gi.test(msg.text)) {
+                bot.sendSticker(id, 'CAACAgIAAxkBAAECl0tg8pGndFDpxClqPUXr9D5Iz-GOZwACuAADHEwXNf-uzxqylPi2IAQ')
+                return
+            }
     })
+}).listen(process.env.PORT || 5000)
+
+console.log('Server running at http://127.0.0.1:5000/');
+//     bot.onText(/\/start/, function onStartText(msg) {
+//         const { id, first_name: userName } = msg.chat
+//         const opts = {
+//           reply_to_message_id: msg.message_id,
+//           reply_markup: JSON.stringify({
+//             keyboard: principles
+//           })
+//         };
+//         bot.sendMessage(id, 'ПРИНЦИПЫ ТРАНСЕРФИНГА РЕАЛЬНОСТИ', opts);
+//         bot.sendMessage(id, `Добро пожаловать ${userName}!`)
+//         setInterval(() => {
+//             bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
+//         }, (1000 * 60 * 60) * hours)
+//     });
+    
+//     bot.on('message', msg => {
+//         const { id } = msg.chat
+//         if(/test/gi.test(msg.text)) {
+//             setInterval(() => {
+//                 bot.sendMessage(id, summary[roundMatch(summary.length - 1, 0)])
+//             }, (1000 * 60 * 60) * hours)
+//         }
+//         if(/start/gi.test(msg.text)) {
+//             return
+//         }
+//         switch (msg.text) {
+//             case '1. «Мир, как зеркало, отражает ваше отношение к нему»':
+//                 bot.sendMessage(id, allPrinciples[0])
+//                 break;
+//             case principles[1][0]:
+//                 bot.sendMessage(id, allPrinciples[1])
+//                 break
+//             case principles[2][0]:
+//                 bot.sendMessage(id, allPrinciples[2])
+//                 break
+//             case principles[3][0]:
+//                 bot.sendMessage(id, allPrinciples[3])
+//                 break
+//             case principles[4][0]:
+//                 bot.sendMessage(id, allPrinciples[4])
+//                 break
+//             case principles[5][0]:
+//                 bot.sendMessage(id, allPrinciples[5])
+//                 break
+//             case principles[6][0]:
+//                 bot.sendMessage(id, allPrinciples[6])
+//                 break
+//             case 'черный': 
+//                 break
+//                 default:
+//                     bot.sendMessage(id, 'Извините такой команды не существует...')
+//                     bot.sendMessage(id, msg.text)
+//                     break;
+//             }
+//             if(/черный/gi.test(msg.text)) {
+//                 bot.sendSticker(id, 'CAACAgIAAxkBAAECl0tg8pGndFDpxClqPUXr9D5Iz-GOZwACuAADHEwXNf-uzxqylPi2IAQ')
+//                 return
+//             }
+//     })
